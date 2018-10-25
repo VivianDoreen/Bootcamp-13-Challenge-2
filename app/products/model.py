4abcc049ddf5f16476da7c7089d4b148e9332621
Nabulo Vivian <nabulovivian2014@gmail.com>
2018-10-25 13:44:47 -0700

    update model file

--- app/products/model.py
+++ app/products/model.py
@@ -1,15 +1,18 @@
 """Product logic for carrying out CRUD (Create, Read, Update, Delete) operations"""
+from flask import abort
 from datetime import datetime
 
 class Product:
     """product class containing CRUD operations of the product"""
     product_code = 1
     products_list = []
-    invalid_id = 'Product not found, please check id'
 
     @staticmethod
     def add_product(pdt_name, pdt_description, pdt_category):
-        """Create a new product record."""
+        """
+        Adds product to list
+        :return: the product that has just been added
+        """
         product = {}
         date = datetime.now()
         name = pdt_name
@@ -31,7 +34,7 @@ class Product:
                     new_product['pdt_category']) == (product['product_name'],
                                                      product['pdt_description'],
                                                      product['pdt_category']):
-                return "Product already exists"
+                abort(409)
             else:
                 product["product_code"] += 1
         Product.products_list.append(new_product)
@@ -39,22 +42,50 @@ class Product:
 
     @staticmethod
     def find_product_by_id(product_id):
-        """Get a specific product using the product’s code"""
-        result_list = [product for product in Product.products_list if product['product_code'] == product_id]
+        """
+        This method gets a single product
+        :param product_id: 
+        :return: single product and status code 200 
+        """
+        result_list =[
+                        product for product in 
+                        Product.products_list
+                        if product['product_code'] == product_id
+                     ]
         if result_list == []:
-            return Product.invalid_id
+            abort(404)
         return result_list
 
     @staticmethod
     def modify_product(product_id, pdt_name, pdt_description, pdt_category):
         """Modify a specific product using the product’s code"""
+        product_result =[
+                        product for product in 
+                        Product.products_list
+                        if product['product_code'] ==  product_id
+                     ]
+        if product_result == []:
+            abort(404)
+        
+        modified_product = {
+                            "product_name" : pdt_name,
+                            "pdt_description" : pdt_description,
+                            "pdt_category" : pdt_category
+                            }
+
         for product in Product.products_list:
-            if product_id == product["product_code"]:
-                product["product_name"] = pdt_name
-                product["pdt_description"] = pdt_description
-                product["pdt_category"] = pdt_category
-                return product
-            return Product.invalid_id
+            if (modified_product['product_name'],
+                    modified_product['pdt_description'],
+                    modified_product['pdt_category']) == (product['product_name'],
+                                                     product['pdt_description'],
+                                                     product['pdt_category']):
+                abort(409)
+        
+            product_result[0]["product_name"] = pdt_name
+            product_result[0]["pdt_description"] = pdt_description
+            product_result[0]["pdt_category"] = pdt_category
+        return product_result
+        
     @staticmethod
     def delete_product(product_id):
         """Delete a specific product using the product’s code"""
@@ -63,11 +94,14 @@ class Product:
                 Product.products_list.remove(product)
                 return "Product deleted"
             
-            return Product.invalid_id
+            abort(404)
 
     @staticmethod
     def list_all_products():
-        """Get all available products"""
+        """
+        This method gets all entries
+        :return: all products in the list
+        """
         if Product.products_list != []:
             return Product.products_list
         return 'No products available'
