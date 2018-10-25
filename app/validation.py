04bc2ae127477e74a4ccec88a8c1da54a07622fc
Nabulo Vivian <nabulovivian2014@gmail.com>
2018-10-25 13:50:15 -0700

    add validation method

---
+++ app/validation.py
@@ -0,0 +1,26 @@
+from cerberus import Validator
+
+class ValidateInput:
+    @staticmethod
+    def validate_input():
+        schema = {
+                    'product_name': {
+                                    'required': True,
+                                    'type': 'string',
+                                    'empty': False,
+                                    'regex': r'^[a-zA-Z]{2,18}(?:[\s_-]{1}[a-zA-Z]+)*$'
+                                    },
+                    'pdt_description': {
+                                    'required': True,
+                                    'type': 'string',
+                                    'empty': False,
+                                    'minlength':2,
+                                    'regex': r'^[a-zA-Z].*[\s.]*$'},
+                    'pdt_category': {
+                                    'required': True,
+                                    'type': 'string',
+                                    'empty': False,
+                                    'minlength':2,
+                                    'regex': r'^[a-zA-Z].*[\s.]*$'}
+                }
+        return Validator(schema)
